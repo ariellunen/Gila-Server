e7ce777c5e2b146aad121f436617ce85e8a92d0f
Chen <chen.nahoom@gmail.com>
2022-01-10 22:40:29 +0200

    zabbix sender for create + linked template

--- handleWebhook.js
+++ handleWebhook.js
@@ -1,68 +1,132 @@
 const axios = require("axios").default;
-const config = require('./config');
-const Zabbix = require('zabbix-promise')
-const URL = config.URL
+const config = require("./config");
+const Zabbix = require("zabbix-promise");
+const URL = config.URL;
 
 const zabbix = new Zabbix({
-    url: config.URL,
-    user: config.User,
-    password: config.Password
-  })
+  url: config.URL,
+  user: config.User,
+  password: config.Password,
+});
 
-  const zabbixSender = async (key, value) => {
-    try {
-      const result = await Zabbix.sender({
-        server: '172.20.10.10', // Same as the address inside the .env
-        host: 'g',
-        key,
-        value,
-      })
-      console.log(result)
-    } catch (error) {
-      console.error(error)
-    }
+const zabbixSender = async (key, value, host) => {
+  try {
+    const result = await Zabbix.sender({
+      server: '172.20.10.10', // Same as the address inside the .env
+      key,
+      value,
+      host
+    })
+    console.log(result)
+  } catch (error) {
+    console.error(error)
   }
+}
 
 module.exports = {
+  createRequest: async (req) => {
+    const hostName = req.queryResult.outputContexts[5].parameters.hostname;
+    const description =
+      req.queryResult.outputContexts[5].parameters.description;
+    try {
+      await zabbix.login();
+      // const zabbixSender = async (Key, Value) => {
+      //   try {
+      //     const result = await Zabbix.sender({
+      //       server: "172.20.10.10", // Same as the address inside the .env
+      //       host: hostName,
+      //       key: Key,
+      //       value: Value,
+      //     });
+      //     console.log(result);
+      //   } catch (error) {
+      //     console.error(error);
+      //   }
+      // };
+      await zabbixSender("req.create.host", 1, 'zserver'); //change to G 
 
-    createRequest: async (req) => {
-        const hostName = req.queryResult.outputContexts[5].parameters.hostname;
-        const description = req.queryResult.outputContexts[5].parameters.description;
-        try {
-            await zabbix.login()
-            await zabbixSender("req.create.host", 1);
+      const groups = await zabbix.request("hostgroup.get", {});
+      const groupId = groups[groups.length - 1].groupid;
 
-            const groups = await zabbix.request('hostgroup.get', {})
-            const groupId = groups[groups.length - 1].groupid
-            const host = await zabbix.request('host.create', {
-              host: hostName,
-              groups: [{ groupid: groupId }],
-              description : description,
-              interfaces: [{
-                type: 1,
-                main: 1,
-                useip: 1,
-                ip: '127.0.0.1',
-                dns: '',
-                port: '10050',
-              }]
-            })
-            console.log(host)
-            
-            await zabbixSender("res.create.host", 1);
-            zabbix.logout()
-          } catch (error) {
-            console.error(error)
-          }
-    },
+      const templateID = await zabbix.request("template.get", {
+        output: "extend",
+        filter: {
+          host: ["Template Monitoring Requests and Responses"],
+        },
+      });
+      //console.log(templateID[0].templateid);
+      const temp = templateID[0].templateid;
 
-    checkRequest: async (body) =>{
-        const requestType = body.queryResult.action;
-        if(requestType.includes("CreateNewHost"))
-        {
-            return "create";
-        }
-        //Need to add else if for the rest of the skills
-        
-    },
-}
+      const host = await zabbix.request("host.create", {
+        host: hostName,
+        groups: [{ groupid: groupId }],
+        description: description,
+        interfaces: [
+          {
+            type: 1,
+            main: 1,
+            useip: 1,
+            ip: "127.0.0.1",
+            dns: "",
+            port: "10050",
+          },
+        ],
+        templates: [
+          {
+            templateid: temp,
+          },
+        ],
+      });
+
+      console.log(host);
+
+      await zabbixSender("res.create.host", 1, 'zserver'); //change to G
+      zabbix.logout();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  checkRequest: async (body) => {
+    const requestType = body.queryResult.action;
+    if (requestType.includes("CreateNewHost")) {
+      return "create";
+    } else if (requestType.includes("problems")) {
+      return "problems";
+    } else {
+      return "failed";
+    }
+  },
+
+  problemsRequest: async (req) => {
+    try {
+      await zabbix.login();
+      const groups = await zabbix.request("hostgroup.get", {});
+      const groupId = groups[groups.length - 1].groupid;
+      const host = await zabbix.request("problem.get", {
+        groups: [{ groupid: groupId }],
+        output: "extend",
+        selectAcknowledges: "extend",
+        selectTags: "extend",
+        selectSuppressionData: "extend",
+        recent: true,
+        sortfield: ["eventid"],
+        sortorder: "DESC",
+        interfaces: [
+          {
+            type: 1,
+            main: 1,
+            useip: 1,
+            ip: "127.0.0.1",
+            dns: "",
+            port: "10050",
+          },
+        ],
+      });
+      console.log(host);
+      zabbix.logout();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
